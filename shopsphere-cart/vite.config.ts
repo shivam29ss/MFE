aeec02edcc7361f42fdecee2549b34a9ec262328
Shivam Srivastava <shivam.srivastava1@actian.com>
2026-09-11 19:05:15 +0530

    updated base url in cart config

--- shopsphere-cart/vite.config.ts
+++ shopsphere-cart/vite.config.ts
@@ -5,25 +5,25 @@ import vue from "@vitejs/plugin-vue";
 import { federation } from "@module-federation/vite";
 
 export default defineConfig({
- plugins: [
-  vue(),
+  plugins: [
+    vue(),
 
-  federation({
-    name: "cart",
-    filename: "remoteEntry.js",
-    manifest: true,
+    federation({
+      name: "cart",
+      filename: "remoteEntry.js",
+      manifest: true,
 
-    exposes: {
-      "./Cart": "./src/cart-entry.ts",
-    },
+      exposes: {
+        "./Cart": "./src/cart-entry.ts",
+      },
 
-    shared: ["vue"],
+      shared: ["vue"],
 
-    library: {
-      type: "module",
-    },
-  }),
-],
+      library: {
+        type: "module",
+      },
+    }),
+  ],
 
   resolve: {
     alias: {
@@ -37,7 +37,7 @@ export default defineConfig({
     origin: "http://localhost:3002",
   },
 
-  base: "/",
+  base: "https://cart-mfe.yrus2909.workers.dev/",
 
   build: {
     target: "esnext",
